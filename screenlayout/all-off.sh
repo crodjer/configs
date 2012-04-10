d00f8b7ac8c67a2e1ac6ed410faee6631dfa6cd5
Rohan Jain <crodjer@gmail.com>
2012-04-10 10:25:19 +0530

    Add screenlayout configs

---
+++ screenlayout/all-off.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --off --output DP1 --off --output VGA1 --off
---
+++ screenlayout/laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 256x1080 --rotate normal --output DP1 --off --output VGA1 --off
