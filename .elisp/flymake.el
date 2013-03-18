8b1d46efdece4a314ca3cbd620b45fdaeb773368
Rohan Jain <crodjer@gmail.com>
2013-03-18 09:33:17 +0530

    Emacs flymake and jshint

--- .elisp/flymake.el
+++ .elisp/flymake.el
@@ -4,7 +4,7 @@
 
 ;; Author:  Pavel Kobyakov <pk_at_work@yahoo.com>
 ;; Maintainer: Sam Graham <libflymake-emacs BLAHBLAH illusori.co.uk>
-;; Version: 0.4.12
+;; Version: 0.4.15
 ;; Keywords: c languages tools
 
 ;; This file is part of GNU Emacs.
@@ -433,7 +433,7 @@ Return its file name if found, or nil if not found."
   (or (flymake-get-buildfile-from-cache source-dir-name)
       (let* ((file (locate-dominating-file source-dir-name buildfile-name)))
         (if file
-            (progn
+            (let* ((file (file-truename file)))
               (flymake-log 3 "found buildfile at %s" file)
               (flymake-add-buildfile-to-cache source-dir-name file)
               file)
@@ -1969,7 +1969,7 @@ Return full-name.  Names are real, not patched."
         (list "-s"
               "-C"
               base-dir
-              (concat "CHK_SOURCES=" source)
+              (concat "CHK_SOURCES=" (shell-quote-argument source))
               "SYNTAX_CHECK_MODE=1"
               "check-syntax")))
 
@@ -1977,7 +1977,7 @@ Return full-name.  Names are real, not patched."
   (list "ant"
         (list "-buildfile"
               (concat base-dir "/" "build.xml")
-              (concat "-DCHK_SOURCES=" source)
+              (concat "-DCHK_SOURCES=" (shell-quote-argument source))
               "check-syntax")))
 
 (defun flymake-simple-make-init-impl (create-temp-f use-relative-base-dir use-relative-source build-file-name get-cmdline-f)
@@ -2163,7 +2163,7 @@ wish to have supplied to Perl -I."
 
 ;;;; xml-specific init-cleanup routines
 (defun flymake-xml-init ()
-  (list "xmlstarlet" (list "val" (flymake-init-create-temp-buffer-copy 'flymake-create-temp-copy))))
+  (list "xmlstarlet" (list "val" "-e" (flymake-init-create-temp-buffer-copy 'flymake-create-temp-copy))))
 
 (provide 'flymake)
 
