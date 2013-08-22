da258888009305d824b120fa39cdf9829b5776b3
Rohan Jain <crodjer@gmail.com>
2013-08-22 17:17:52 +0530

    Improve xmobar config
    Use my compiled version again

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -81,7 +81,7 @@ layoutHook' = customLayout
 customPP :: PP
 customPP = defaultPP { ppCurrent = xmobarColor "#FFEE00" "" . wrap "[" "]"
                      , ppVisible = xmobarColor "#5599FF" "" . wrap "<" ">"
-                     , ppTitle =  shorten 70
+                     , ppTitle =  shorten 80
                      , ppSep =  "<fc=#AFAF87>|</fc>"
                      , ppHiddenNoWindows = xmobarColor "#404040" ""
                      , ppUrgent = xmobarColor "#ff0000" "" . wrap "!" "!"
