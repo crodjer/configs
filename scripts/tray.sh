18aa041c55345defa4d8581c164dab5a64352439
Rohan Jain <crodjer@gmail.com>
2015-02-07 10:48:10 +0530

    stalonetray config

--- scripts/tray.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pkill trayer
-sleep 0.1
-trayer --edge top --SetDockType true --align right --height 14 \
-       --widthtype request --tint 0x555555 --transparent true --alpha 0
