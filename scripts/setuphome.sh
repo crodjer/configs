0e152bfc310da4b0fc6c0e888d3109752e88f318
Rohan Jain <code@rohanjain.in>
2015-03-15 22:02:55 +0530

    Add zathura configuration

--- scripts/setuphome.sh
+++ scripts/setuphome.sh
@@ -171,6 +171,7 @@ setUp(){
     # $HOME/.config directroy for programs which store conf here
     mkHomeConfDir config
     link config/awesome .config/awesome _parent
+    link config/zathura .config/zathura _parent
     link .mplayer .mplayer _parent
 
     # $HOME/.xmonad directroy for xmonad configs
