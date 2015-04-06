a48a6362d63c6ae011aa5914e72a366fa51b3042
Rohan Jain <code@rohanjain.in>
2015-04-06 12:34:00 +0530

    Run jacked checking script via absolute path.

--- scripts/hourly-reminder.sh
+++ scripts/hourly-reminder.sh
@@ -5,6 +5,8 @@
 #
 #     0,30 * * * * /path/to/this/script/hourly-reminder.sh
 
+script_dir=$(dirname "$0")
+
 hours=$(date +%H)
 minutes=$(date +%M)
 # notification="$hours:$minutes"
@@ -12,7 +14,7 @@ title="$hours:$minutes"
 
 allow_beep="true"
 
-jacked.sh && {
+$script_dir/jacked.sh && {
     unset allow_beep
 }
 
@@ -24,11 +26,14 @@ case $minutes in
          beep_args="-f 250 -l 400"
          ;;
      *)
+         beep_args="-f 300 -l 100"
          ;;
 esac
 
+echo $allow_beep
+
 export DISPLAY=:0
-notify-send "$title" # "$notification"
+# notify-send "$title" # "$notification"
 
 if [[ $beep_args && $allow_beep ]]; then
     beep $beep_args
