5ca23bef4c6115c87a4d88a69ec4fb3d5ccbcf7d
Rohan Jain <code@rohanjain.in>
2015-07-09 11:26:07 +0530

    Verify that the target is a mounted fs before backing up

--- scripts/backup-system.sh
+++ scripts/backup-system.sh
@@ -2,8 +2,8 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 EXCLUDE_FROM=$DIR/../.backup-excludes.txt
-TARGET_HOME="$1"
-TARGET_ROOT="$2"
+TARGET_HOME="${1%/}"
+TARGET_ROOT="${2%/}"
 EXECUTE="$3"
 
 rsync_opts="-valrhpoxt --one-file-system --exclude-from=$EXCLUDE_FROM"
@@ -20,6 +20,15 @@ if [[ "$TARGET_ROOT" != /mnt/* || -z "$TARGET_HOME" ]]; then
     echo "Require both target home and target root!" >&2
     exit 1
 fi
+
+df | grep $TARGET_ROOT > /dev/null || {
+    echo "Target root should be a mounted partition." >&2
+    exit 1
+}
+df | grep $TARGET_HOME > /dev/null || {
+    echo "Target home should be a mounted partition." >&2
+    exit 1
+}
 
 backup /home/ $TARGET_HOME
 backup / $TARGET_ROOT
