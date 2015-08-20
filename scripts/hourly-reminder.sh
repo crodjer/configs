60442943a302f39377902d7f0670b22c6134d740
Rohan Jain <code@rohanjain.in>
2015-08-20 07:53:05 +0530

    Update xsessionrc, reminder script
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- scripts/hourly-reminder.sh
+++ scripts/hourly-reminder.sh
@@ -6,19 +6,14 @@
 #     0,30 * * * * /path/to/this/script/hourly-reminder.sh
 
 script_dir=$(dirname "$0")
-
 hours=$(date +%H)
 minutes=$(date +%M)
 title="$hours:$minutes"
 
-if [[ $(ps -ef | grep -v grep | grep "xserverrc" | grep " :0 ") ]]; then
-    export DISPLAY=:0
-elif [[ $(ps -ef | grep -v grep | grep "xserverrc" | grep " :1 ") ]]; then
-    export DISPLAY=:1
-fi
+export DISPLAY=$(pgrep -fa xserverrc | cut -d " " -f 6)
 
 if [ $(command -v task) ]; then
     notification="Pending: $(task status:pending count)"
 fi
 
-alert.sh $title $notification
+$script_dir/alert.sh $title $notification &>> /tmp/hourly-reminder.log
