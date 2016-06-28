10bc0f543b2c8596d3097e019442d7b65cf09a57
Rohan Jain <crodjer@gmail.com>
2016-06-28 12:33:39 +0530

    Update tmux configuration

--- scripts/tmux-config.sh
+++ scripts/tmux-config.sh
@@ -4,12 +4,16 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 configs_dir="$dir/.."
 
 if [[ $(uname -a) =~ Darwin ]]; then
-    # As usual, we need to handle a
+    # As usual, we need to handle OSX quirks.
     session=$(hostname | cut -d '.' -f 1)
-    # Top in OSX
-    top_command=htop
 else
     session=$(hostname)
+fi
+
+if [ $(command -v htop) ]; then
+    # System may have issues with top, hence htop is available. Use that.
+    top_command=htop
+else
     top_command=top
 fi
 
@@ -18,18 +22,11 @@ attach-windows () {
     send-keys                          # 0
     tmux split-window -hc $configs_dir # 0 (split)
     tmux new-window                    # 1
-    tmux new-window                    # 2
-    tmux new-window                    # 3
-    tmux new-window                    # 4
-    tmux new-window                    # 5
-    tmux new-window                    # 6
-    tmux new-window                    # 7
-    tmux new-window                    # 8
-    tmux new-window $top_command       # 9
-    tmux split-window -h               # 9 (secondary monitoring)
-    tmux split-window -v               # 9 (tertiary monitoring)
-
-    select-window -t :0
+    tmux new-window -t 9 $top_command  # 9
+    tmux split-window -t 9 -h          # 9 (secondary monitoring)
+    tmux split-window -t 9 -v          # 9 (tertiary monitoring)
+    tmux select-window -t :0
+    tmux select-pane -t 0
 }
 
 tmux attach-session -t $session || {
