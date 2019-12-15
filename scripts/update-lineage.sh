ea10aefe24c5142fc49158fff954a2a1b16543d3
Rohan Jain <crodjer@gmail.com>
2019-12-15 22:48:33 +0530

    Update linage install script to auto install and block ads

--- scripts/update-lineage.sh
+++ scripts/update-lineage.sh
@@ -26,10 +26,10 @@ mkdir -p $work_dir
 
 cd $work_dir
 echo "Downloading LineageOS for $codename..."
-wget -nc "$lineage_url"
+aria2c -c "$lineage_url"
 
 echo "Downloading OpenGApps for $GAPPS_PLATFORM:$GAPPS_VERSION:$GAPPS_VARIENT..."
-wget -nc "$gapps_url"
+aria2c -c "$gapps_url"
 
 lineage_zip=$(basename $lineage_url)
 gapps_zip=$(basename $gapps_url)
@@ -38,14 +38,29 @@ if [ "$(curl -s "$lineage_url?sha256")" !=  "$(sha256sum $lineage_zip)" ]; then
     >&2 echo "LineageOS SHA256 Sum Mismatch!"
     exit 1
 fi
-
-# if [ "$(curl -sL $gapps_url.md5)" !=  "$(md5sum $gapps_zip)" ]; then
-#     >&2 echo "OpenGApps SHA256 Sum Mismatch!"
-#     exit 1
-# fi
+if [ "$(curl -sL $gapps_url.md5)" !=  "$(md5sum $gapps_zip)" ]; then
+    >&2 echo "OpenGApps SHA256 Sum Mismatch!"
+    exit 1
+fi
 
 echo "Please reboot your phone into recovery connect to this machine."
 read -p "Press enter once done..."
 
 echo "Pushing zips to recovery via adb..."
 adb push $lineage_zip $gapps_zip /tmp/
+
+echo "Installing Lineage"
+adb shell twrp install $lineage_zip
+echo "Installing GApps"
+adb shell twrp install $gapps_zip
+
+echo "Cleaning Cache/Dalvik"
+adb shell twrp wipe cache
+adb shell twrp wipe dalvik
+
+echo "Updating Hosts to block ads."
+adb shell mount -o rw /dev/block/bootdevice/by-name/system /system
+adb shell cp /system/etc/hosts /system/etc/hosts.bk
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O $work_dir/hosts
+adb push $work_dir/hosts /system/etc/hosts
+adb shell umount /system
