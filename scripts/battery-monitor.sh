6045506d657913af1c734dbdd8252787cacf6abf
Rohan Jain <crodjer@gmail.com>
2019-12-30 16:40:02 +0530

    Make battery monitor work correctly with DBUS

--- scripts/battery-monitor.sh
+++ scripts/battery-monitor.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env sh
 
+## Add to cron to ensure it is run every minute. Eg:
+# * * * * * sudo /home/rohan/configs/scripts/battery-monitor.sh
+
+user_name="rohan"
+
 acpi -b  | cut -d ' ' -f 3,4 | sed 's/[,%]//g' | {
     read -r status capacity
 
-
-    if [ "$status" = Discharging -a "$capacity" -le 5 ]; then
-        # Set enviornment so that `notify-send` works.
-        _DBUS_ENV="$(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u rohan dbus | head -1)/environ)"
-        export DISPLAY=:0
-        eval "export $_DBUS_ENV"
-
-        sudo -u rohan /usr/bin/notify-send --urgency critical \
+    if [ "$status" = Discharging -a "$capacity" -le 10 ]; then
+        sudo -u $user_name \
+            DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $user_name)/bus \
+            DISPLAY=:0 \
+            /usr/bin/notify-send --urgency critical \
             "⚠ Battery Low!" \
-            "Capcity: $capacity%. Going to sleep in 15 seconds."
+            "Capcity: $capacity%. Going to sleep in 30 seconds."
+
         logger "Critical battery threshold"
-        sleep 15
-        systemctl hybrid-sleep
+        sleep 30
+        systemctl suspend
     fi
 }
-
-
