226ab713cea62ef2d9362e87958dded72b9e230a
Rohan Jain <crodjer@gmail.com>
2020-05-31 09:41:42 +0530

    RaspberryPi friendly updates

--- scripts/update-rust-projects.sh
+++ scripts/update-rust-projects.sh
@@ -8,10 +8,23 @@ function run_command {
     bash -c "$cmd" &> /tmp/cron-$log.log
 }
 
-# run_command "docker container prune -f && docker image prune -af" docker-cleanup
-run_command "docker container prune -f"  docker-container-cleanup
-run_command "$HOME/.cargo/bin/rustup update"  rustup-update
-run_command "$HOME/.cargo/bin/cargo +nightly install-update racer"  racer-update
-run_command "$HOME/.cargo/bin/cargo install-update -a"  cargo-update
-run_command "cd $HOME/projects/alacritty && git pull && cargo build --release"  alacritty-update
-run_command "cd $HOME/projects/tmux && git pull && ./autogen.sh && make"  tmux-update
+if [ -x "$(command -v docker)" ]; then
+  run_command "docker container prune -f && docker image prune -af" docker-cleanup
+fi
+
+if [ -x "$(command -v rustup)" ]; then
+    run_command "$HOME/.cargo/bin/rustup update"  rustup-update
+fi
+
+if [ -x "$(command -v cargo)" ]; then
+    run_command "$HOME/.cargo/bin/cargo +nightly install-update racer"  racer-update
+    run_command "$HOME/.cargo/bin/cargo install-update -a"  cargo-update
+fi
+
+if [ -d $HOME/projects/alacritty/ ]; then
+    run_command "cd $HOME/projects/alacritty && git pull && cargo build --release"  alacritty-update
+fi
+
+if [ -d $HOME/projects/tmux/ ]; then
+    run_command "cd $HOME/projects/tmux && git pull && ./autogen.sh && make"  tmux-update
+fi
