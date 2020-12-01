67ebc89efc256dc9eaf9578d37fde88cc3d21717
Rohan Jain <crodjer@gmail.com>
2020-12-01 20:13:34 +0530

    RPi Fan: Modualirized and Improved Logging

--- scripts/rpi-fan.py
+++ scripts/rpi-fan.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+import RPi.GPIO as GPIO
+import signal
 import sys
 import time
-import RPi.GPIO as GPIO
 
-# Source: https://fizzy.cc/raspberry-pi-fan/
+from datetime import datetime
+
 # Recommend setting up a systemd service like this:
 # /etc/systemd/system/fan-control.service
 #
@@ -20,40 +22,103 @@ import RPi.GPIO as GPIO
 # [Install]
 # WantedBy=multi-user.target
 
-FAN_PIN = 18
+class FanControl:
 
-def cpu_temp():
-    with open("/sys/class/thermal/thermal_zone0/temp", 'r') as f:
-        return float(f.read())/1000
+    monitoring = True
+    last_event = None
+    fan_pin = None
+    low = 48.0
+    high = 55.0
+
+    def __init__(self, fan_pin, low=None, high=None):
+        self.fan_pin = fan_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.fan_pin, GPIO.OUT)
+
+        if low:
+            self.low = low
+        if high:
+            self.high = high
+
+        signal.signal(signal.SIGINT, self.exit)
+        signal.signal(signal.SIGTERM, self.exit)
+        self.last_event = datetime.now()
+        print(self.pretty_state(), file=sys.stderr)
+
+    @property
+    def is_on(self):
+        return GPIO.input(self.fan_pin) == 1
+
+    @property
+    def temp(self):
+        with open("/sys/class/thermal/thermal_zone0/temp", 'r') as f:
+            return float(f.read())/1000
+
+    @property
+    def state(self):
+        return 'On' if self.is_on else 'Off'
+
+    def pretty_state(self):
+        return "{} @ {:0.2f}'C".format(self.state, self.temp)
 
 
-def main():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(FAN_PIN,GPIO.OUT)
+    def watch(self):
 
-    is_close = True
-    GPIO.output(FAN_PIN,GPIO.LOW)
+        while self.monitoring:
+            temp = self.temp
 
-    print("Starting temprature monitring and fan control.")
+            if temp >= self.high:
+                self.on()
+            elif temp < self.low:
+                self.off()
 
-    while True:
-        temp = cpu_temp()
-        if is_close:
-            if temp > 55.0:
-                print(time.ctime(), temp, 'Fan ON', file=sys.stderr)
-                GPIO.output(FAN_PIN,GPIO.HIGH)
-                is_close = False
-        else:
-            if temp < 48.0:
-                print(time.ctime(), temp, 'Fan OFF', file=sys.stderr)
-                GPIO.output(FAN_PIN,GPIO.LOW)
-                is_close = True
+            # print(self.pretty_state(), file=sys.stderr)
+            time.sleep(5.0)
 
-        time.sleep(5.0)
+        print('Done!', file=sys.stderr)
 
-        print(time.ctime(), temp, file=sys.stderr)
+    def exit(self, _signum, _frame):
+        self.off()
+        self.monitoring = False
+        print('Exiting...', file=sys.stderr)
 
+    def duration(self, prev_state):
+        if not self.last_event:
+            return 'Duration N/A'
+
+        last_event = self.last_event
+        now = datetime.now()
+        self.last_event = now
+
+        return 'Was {} for: {}'.format(prev_state, (now - last_event))
+
+    def off(self):
+        if not self.is_on:
+            return
+
+        prev_state = self.state
+        GPIO.output(self.fan_pin, GPIO.LOW)
+
+        print("{} ({})".format(
+            self.pretty_state(),
+            self.duration(prev_state)
+        ), file=sys.stderr)
+
+    def on(self):
+        if self.is_on:
+            return
+
+        prev_state = self.state
+        GPIO.output(self.fan_pin, GPIO.HIGH)
+
+        print("{} ({})".format(
+            self.pretty_state(),
+            self.duration(prev_state)
+        ), file=sys.stderr)
 
 if __name__ == '__main__':
-    main()
+    print("Starting temprature monitring and fan control...")
+    controller = FanControl(18)
+    controller.watch()
