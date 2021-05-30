13c1d0e7d795cda3bf442521f490f370b2abd783
Rohan Jain <crodjer@gmail.com>
2021-05-30 14:46:28 +0530

    Scripts to manage autojump and neovim

--- scripts/update-projects.sh
+++ scripts/update-projects.sh
@@ -8,6 +8,8 @@ function run_command {
   bash -c "$cmd" &> /tmp/update-projects-$log.log
 }
 
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 if [ -x "$(command -v docker)" ]; then
   run_command "docker container prune -f && docker image prune -af" docker-cleanup
 fi
@@ -21,16 +23,10 @@ if [ -x "$(command -v cargo)" ]; then
   run_command "cargo install-update -a"  cargo-update
 fi
 
-if [ -d $HOME/projects/alacritty/ ]; then
-  run_command "cd $HOME/projects/alacritty && git pull && cargo build --release"  alacritty-update
-fi
-
-if [ -d $HOME/projects/tmux/ ]; then
-  run_command "cd $HOME/projects/tmux && git pull && ./autogen.sh && make"  tmux-update
+if [ -d $HOME/projects/neovim/ ]; then
+  run_command "cd $HOME/projects/neovim && git pull && make install"  neovim-update
 fi
 
 if [ -x "$(command -v autojump)" ]; then
-  LOCAL_PROFILE_DIR=$HOME/.config/profile.d/
-  mkdir -p $LOCAL_PROFILE_DIR
-  run_command "cd $LOCAL_PROFILE_DIR && wget -c https://raw.githubusercontent.com/wting/autojump/master/bin/autojump.{zsh,bash}" autojump-scripts-update
+  run_command "$SCRIPTS_DIR/install-autojump.sh" autojump-update
 fi
