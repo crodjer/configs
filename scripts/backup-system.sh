7466b7fb344548e99e488740ca321dd4a5d3f7bd
Rohan Jain <code@rohanjain.in>
2021-06-10 17:42:28 +0530

    Script to automatically backup a remote/local system on Disk

---
+++ scripts/backup-system.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+DEVICE=$1
+
+function log() {
+    echo "[$(date -Iseconds)][${DEVICE:-unknown}]" $@
+}
+
+if [ -z "$DEVICE" ]; then
+    SOURCE="/"
+    DEVICE=$(hostname)
+    SELF=true
+else
+    SOURCE="$DEVICE:/"
+fi
+
+log "Backing up $DEVICE!"
+
+TARGET=/mnt/external/safe/backup/$DEVICE/
+
+if [ -z "$SELF" ]; then
+    ssh $DEVICE 'test -d /home/rohan/.ssh' || {
+        log "Home directry not accessible on $DEVICE!"
+            exit 1
+        }
+fi
+
+rsync -aAXH \
+    --progress \
+    --delete \
+    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/var/log/*","/swapfile","/lost+found"} \
+    $SOURCE $TARGET
