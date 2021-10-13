07dc55688e1a762040d76677faead2c1da8537a4
Rohan Jain <code@rohanjain.in>
2021-10-13 17:37:28 +0530

    Move to wofi

--- scripts/update-projects.sh
+++ scripts/update-projects.sh
@@ -26,10 +26,6 @@ if [ -d $HOME/projects/forks/gnvim/ ]; then
   run_command "cd $HOME/projects/forks/gnvim && git pull && make && PREFIX=~/.local make install"  neovim-update
 fi
 
-if [ -d $HOME/projects/forks/yofi/ ]; then
-  run_command "cd $HOME/projects/forks/yofi && git pull && cargo build --release"  yofi-update
-fi
-
 if [ -x "$(command -v autojump)" ]; then
   run_command "$SCRIPTS_DIR/install-autojump.sh" autojump-update
 fi
