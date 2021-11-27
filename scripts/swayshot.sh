bbe7106af0968f1d59e71206de8202d6887a74a7
Rohan Jain <code@rohanjain.in>
2021-11-27 23:20:53 +0530

    Some sway realted tweaks

--- scripts/swayshot.sh
+++ scripts/swayshot.sh
@@ -2,4 +2,5 @@
 
 # Borrowed from: https://jolyonbrown.com/post/sway-screenshots/
 filename="screenshot-`date +%F-%T`"
-grim -g "$(slurp)" ~/downloads/screenshots/$filename.png
+mkdir -p ~/pictures/screenshots
+grim -g "$(slurp)" ~/pictures/screenshots/$filename.png
