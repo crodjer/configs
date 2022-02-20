2e7373b10d6e8c1fc6904b6fbd8e0dacbf20d808
Rohan Jain <crodjer@pm.me>
2022-02-20 18:33:03 +0530

    Ensure leaner backups with backup script

--- scripts/backup-system.sh
+++ scripts/backup-system.sh
@@ -18,6 +18,7 @@ fi
 log "Backing up $DEVICE!"
 
 TARGET=${TARGET:-/mnt/external/safe/backup/$DEVICE/}
+HOME_DIR=${HOME_DIR:-/home/rohan}
 
 if [ -z "$SELF" ]; then
     ssh $DEVICE 'test -d /home/rohan/.ssh' || {
@@ -26,12 +27,44 @@ if [ -z "$SELF" ]; then
         }
 fi
 
-EXTRA_ARGS=$(ssh $DEVICE -- cat /home/rohan/.backup-rsync-params 2> /dev/null | tr '\n' ' ' || echo "" )
+if [ -z "$SELF" ]; then
+    EXTRA_ARGS=$(ssh $DEVICE -- cat $HOME_DIR/.backup-rsync-params 2> /dev/null | tr '\n' ' ' || echo "" )
+else
+    EXTRA_ARGS=$(cat $HOME_DIR/.backup-rsync-params 2> /dev/null | tr '\n' ' ' || echo "" )
+fi
 
-rsync -aAXH \
+EXCLUDES_FILE=$(mktemp -u --suffix=-rsync-backup-excludes)
+
+# Generally excluded paths.
+cat > $EXCLUDES_FILE <<- EOM
+/dev/*
+/proc/*
+/sys/*
+/tmp/*
+/run/*
+/mnt/*
+/media/*
+/var/log/*
+/var/tmp/*
+/swapfile
+/lost+found
+/root/.cache
+/.snapshots
+/var/lib/flatpak/runtime/*
+/var/lib/flatpak/repo/*
+/var/lib/flatpak/.removed/*
+$HOME_DIR/.cache/*
+$HOME_DIR/.cargo/git/*
+$HOME_DIR/.cargo/registry/*
+$HOME_DIR/.rustup/toolchains/*
+EOM
+
+sudo rsync -aAXH \
     --progress \
     --delete \
     --delete-excluded \
-    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/var/log/*","/var/tmp/*","/swapfile","/lost+found"} \
+    --exclude-from=$EXCLUDES_FILE \
     $EXTRA_ARGS \
     $SOURCE $TARGET
+
+rm $EXCLUDES_FILE
