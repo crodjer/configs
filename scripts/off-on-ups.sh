4293a2b7913c8de997885f7a0c902c1301cfd1b6
Rohan Jain <crodjer@pm.me>
2023-05-09 13:26:33 +0530

    Script to shut down a system when power out
    It relies on the fact that the various IOT devices likely went offline
    because of a power outage.

---
+++ scripts/off-on-ups.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+log() {
+    echo "[$(date -Iseconds)]" $@ >&2
+}
+
+usage()
+{
+cat << EOF
+usage: $0 <device> <device>...
+
+Safely shut down the system when the power at home is off and the computer is
+running on the UPS.
+EOF
+}
+
+SHUTDOWN=true
+
+DEVICES=($@)
+
+if [ -z "${DEVICES[0]}" ]; then
+    log "At least 1 device to test with is required!"
+    usage
+    exit 1
+fi
+
+is_online() {
+    ip=$1
+    if ping -c 3 -i 0.2 -w 1 $1 >/dev/null 2>&1
+    then
+        log $1 is online!
+        return 0
+    else
+        log $1 is offline!
+        return 1
+    fi
+}
+
+for device in "${DEVICES[@]}"; do
+    if is_online $device; then
+        SHUTDOWN=false
+    fi
+done
+
+if $SHUTDOWN; then
+    log "Power's likely out! Shutting down."
+    sudo poweroff
+fi
