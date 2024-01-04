3e322a351e2b4606ace553eefb0b9688e0bc7153
Rohan Jain <code@rohanjain.in>
2024-01-04 19:51:18 +0530

    Add configuration for wezterm!

---
+++ .wezterm.lua
@@ -0,0 +1,68 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.window_decorations = "NONE"
+
+-- Split / Pane navigation bindings similar to my tmux bindings.
+config.keys = {
+  {
+    key = '\\',
+    mods = 'ALT',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '\'',
+    mods = 'ALT',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'h',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+}
+
+
+-- OS Specific Configuration
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
+if is_darwin() then
+  config.font = wezterm.font_with_fallback {
+    'Menlo',
+  }
+  config.font_size = 13.5
+
+  -- Mac's handle dark screen well through dimming.
+  config.color_scheme = "Catppuccin Mocha"
+end
+
+if is_linux() then
+  -- On my linux machine, its actually eco friendly to use it in light mode
+  -- but with low brightness.
+  config.color_scheme = "Catppuccin Latte"
+end
+
+
+return config
