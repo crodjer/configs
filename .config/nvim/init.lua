ad92fb8825d40c2795ee8207d5a26affa54bf910
Rohan Jain <crodjer@pm.me>
2024-01-09 17:13:04 +0530

    Cleanup `nvim` configugration
    Remove unnecessary things imported from kickstarter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,30 +42,27 @@ if not package.loaded["lazy"] then
       'neovim/nvim-lspconfig',
       dependencies = {
         -- Useful status updates for LSP
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
         { 'j-hui/fidget.nvim', opts = {} },
 
         -- Additional lua configuration, makes nvim stuff amazing!
-        'folke/neodev.nvim',
+        {
+          'folke/neodev.nvim',
+          filetype = 'lua',
+          opts = {
+            override = function(root_dir, library)
+              -- Since I symlink my init.lua, neodev doesn't detect this right.
+              if root_dir:sub(- #"nvim") == "nvim" then
+                library.enabled = true
+                library.plugins = true
+              end
+            end
+          },
+        },
       },
     },
 
-    {
-      -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-
-        -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-      },
-    },
-
-    -- Useful plugin to show you pending keybinds.
     { 'folke/which-key.nvim',  opts = {} },
+
     {
       -- Adds git related signs to the gutter, as well as utilities for managing changes
       'lewis6991/gitsigns.nvim',
@@ -78,80 +75,17 @@ if not package.loaded["lazy"] then
           topdelete = { text = '‾' },
           changedelete = { text = '~' },
         },
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-
-          -- Navigation
-          map({ 'n', 'v' }, ']c', function()
-            if vim.wo.diff then
-              return ']c'
-            end
-            vim.schedule(function()
-              gs.next_hunk()
-            end)
-            return '<Ignore>'
-          end, { expr = true, desc = 'Jump to next hunk' })
-
-          map({ 'n', 'v' }, '[c', function()
-            if vim.wo.diff then
-              return '[c'
-            end
-            vim.schedule(function()
-              gs.prev_hunk()
-            end)
-            return '<Ignore>'
-          end, { expr = true, desc = 'Jump to previous hunk' })
-
-          -- Actions
-          -- visual mode
-          require('which-key').register {
-            ['<leader>gh'] = { name = '[G]it [H]unk', _ = 'which_key_ignore' },
-          }
-          map('v', '<leader>ghs', function()
-            gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-          end, { desc = 'stage git hunk' })
-          map('v', '<leader>ghr', function()
-            gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-          end, { desc = 'reset git hunk' })
-          -- normal mode
-          map('n', '<leader>ghs', gs.stage_hunk, { desc = 'git stage hunk' })
-          map('n', '<leader>ghr', gs.reset_hunk, { desc = 'git reset hunk' })
-          map('n', '<leader>ghS', gs.stage_buffer, { desc = 'git Stage buffer' })
-          map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-          map('n', '<leader>ghR', gs.reset_buffer, { desc = 'git Reset buffer' })
-          map('n', '<leader>ghp', gs.preview_hunk, { desc = 'preview git hunk' })
-          map('n', '<leader>ghb', function()
-            gs.blame_line { full = false }
-          end, { desc = 'git blame line' })
-          map('n', '<leader>ghd', gs.diffthis, { desc = 'git diff against index' })
-          map('n', '<leader>ghD', function()
-            gs.diffthis '~'
-          end, { desc = 'git diff against last commit' })
-
-          -- Toggles
-          map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-          map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-          -- Text object
-          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-        end,
       },
     },
 
     {
       'catppuccin/nvim',
-      priority = 1000,
       config = function()
         vim.cmd.colorscheme 'catppuccin'
-        require('auto-dark-mode').setup()
       end,
-      dependencies = { 'f-person/auto-dark-mode.nvim' }
+      dependencies = {
+        { 'f-person/auto-dark-mode.nvim', opts = {} }
+      }
     },
 
     {
@@ -160,7 +94,6 @@ if not package.loaded["lazy"] then
       -- See `:help lualine.txt`
       opts = {
         options = {
-          icons_enabled = true,
           theme = 'catppuccin',
           section_separators = { left = '', right = '' },
           component_separators = { left = '', right = '' }
@@ -180,34 +113,11 @@ if not package.loaded["lazy"] then
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
-    -- Fuzzy Finder (files, lsp, etc)
     {
-      'nvim-telescope/telescope.nvim',
-      branch = '0.1.x',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          -- NOTE: If you are having trouble with this installation,
-          --       refer to the README for telescope-fzf-native for more instructions.
-          build = 'make',
-          cond = function()
-            return vim.fn.executable 'make' == 1
-          end,
-        },
-      },
-    },
-
-    {
-      -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-      },
-      build = ':TSUpdate',
+      "ibhagwan/fzf-lua",
+      -- optional for icon support
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      opts = {}
     },
 
     {
@@ -219,12 +129,13 @@ if not package.loaded["lazy"] then
     {
       'pearofducks/ansible-vim',
       build = 'UltiSnips/generate.sh',
-      ft = 'yaml'
-    },
-
-    {
-      'akinsho/toggleterm.nvim',
-      version = "*"
+      ft = 'yaml',
+      config = function()
+        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+          pattern = { "*/plays/*.yaml*" },
+          command = "set filetype=yaml.ansible",
+        })
+      end
     },
 
     {
@@ -294,13 +205,27 @@ vim.o.colorcolumn = "+1"
 
 -- [[ Basic Keymaps ]]
 
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- [[ FZF Keymaps ]]
+local fzf = require('fzf-lua')
 
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+local function map(binding, mapping, desc)
+  vim.keymap.set('n', '<leader>' .. binding, mapping, {
+    noremap = true, silent = true, desc = desc
+  })
+end
+
+map('f', function()
+  local git_dir = vim.fn.finddir('.git', vim.fn.getcwd() .. ";")
+  if git_dir == '' then
+    fzf.files()
+  else
+    fzf.git_files()
+  end
+end, "Search [F]iles")
+map('b', fzf.buffers, "Search [B]uffers")
+map('h', fzf.oldfiles, "Search [H]istory")
+map('sl', fzf.live_grep, "[S]earch [L]ive Grep")
+map('sc', fzf.commands, "[S]earch [C]ommands")
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
@@ -319,156 +244,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-local actions = require("telescope.actions")
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
-local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
-
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
-end
-
--- Custom live_grep function to search in git root
-local function live_grep_git_root()
-  local git_root = find_git_root()
-  if git_root then
-    require('telescope.builtin').live_grep {
-      search_dirs = { git_root },
-    }
-  end
-end
-
-vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>h', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files, { desc = 'Search Git [F]iles' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-local function telescope_live_grep_open_files()
-  require('telescope.builtin').live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
-end
-vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sc', require('telescope.builtin').command_history, { desc = '[S]earch [C]ommand history' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
--- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'ledger' },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = true,
-
-    highlight = { enable = true },
-    indent = { enable = true, disable = { "ledger", "ruby" } },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
-  }
-end, 0)
-
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
@@ -482,13 +257,6 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>cr', vim.lsp.buf.rename, '[C]ode [R]ename')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -507,17 +275,6 @@ local on_attach = function(_, bufnr)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
-
--- Setup neovim lua configuration
-require('neodev').setup({
-  override = function(root_dir, library)
-    -- Since I symlink my init.lua, neodev doesn't detect this right.
-    if root_dir:sub(-#"nvim") == "nvim" then
-      library.enabled = true
-      library.plugins = true
-    end
-  end,
-})
 
 local lspconfig = require('lspconfig');
 local servers = {
@@ -550,114 +307,6 @@ require('which-key').register {
 require('which-key').register({
   ['<leader>'] = { name = 'VISUAL <leader>' },
 }, { mode = 'v' })
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require("luasnip.loaders.from_snipmate").lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = {
-    completeopt = 'menu,menuone,noinsert',
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  },
-}
-
--- [[ Configure ansible patterns ]]
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*/plays/*.yaml*" },
-  command = "set filetype=yaml.ansible",
-})
-
--- [[ Configure window bindings ]]
--- [[ Configure terminal ]]
-require('toggleterm').setup {
-  shade_terminals = true,
-  shading_factor = -0.5,
-}
-vim.keymap.set('n', '<C-Enter>', ':ToggleTerm size=25<CR>', { desc = 'Open terminal 1' })
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('n', 'q', ':hide<CR>', opts)
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-Enter>', [[<C-\><C-n>:hide<CR>]], opts)
-  vim.keymap.set('t', '<C-w>h', [[<C-\><C-n><C-w>h]], opts)
-  vim.keymap.set('t', '<C-w>j', [[<C-\><C-n><C-w>j]], opts)
-  vim.keymap.set('t', '<C-w>k', [[<C-\><C-n><C-w>k]], opts)
-  vim.keymap.set('t', '<C-w>l', [[<C-\><C-n><C-w>l]], opts)
-end
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = "term://*",
-  command = "startinsert"
-})
-
-vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  pattern = "term://*",
-  command = "lua set_terminal_keymaps()"
-})
-
-local terminal = require('toggleterm.terminal').Terminal
-local terminalApps = {
-  bottom = {
-    binding = "m",
-    terminal = { cmd = "btm --color nord-light", direction = "float" },
-    desc = "System [M]onitor (using btm)"
-  }
-}
-for _, config in pairs(terminalApps) do
-  local app = terminal:new(config.terminal)
-  local handler = function()
-    app:toggle()
-  end
-  vim.keymap.set("n", "<leader>$" .. config.binding, handler, {
-    noremap = true, silent = true, desc = "[$]hell: " .. config.desc
-  })
-end
 
 -- If custom overrides exist, load them.
 pcall(require, 'custom')
