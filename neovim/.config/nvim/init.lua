3b13672e22152faa1ede2fd5e9c6462aa9ca03e2
Rohan Jain <code@rohanjain.in>
2024-10-29 19:06:20 +0530

    Improve `neovim` LSP configuration

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -66,8 +66,71 @@ if not package.loaded["lazy"] then
             end
           },
         },
+
+        'hrsh7th/cmp-nvim-lsp',
       },
     },
+
+    { -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      event = 'InsertEnter',
+      dependencies = {
+        -- Adds other completion capabilities.
+        --  nvim-cmp does not ship with all sources by default. They are split
+        --  into multiple repos for maintenance purposes.
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
+      },
+      config = function()
+        -- See `:help cmp`
+        local cmp = require 'cmp'
+
+        cmp.setup {
+          completion = { completeopt = 'menu,menuone,noinsert' },
+
+          -- For an understanding of why these mappings were
+          -- chosen, you will need to read `:help ins-completion`
+          --
+          -- No, but seriously. Please read `:help ins-completion`, it is really good!
+          mapping = cmp.mapping.preset.insert {
+            -- Select the [n]ext item
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            -- Select the [p]revious item
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+            -- Scroll the documentation window [b]ack / [f]orward
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+            -- Accept ([y]es) the completion.
+            --  This will auto-import if your LSP supports it.
+            --  This will expand snippets if the LSP sent a snippet.
+            ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+            -- If you prefer more traditional completion keymaps,
+            -- you can uncomment the following lines
+            --['<CR>'] = cmp.mapping.confirm { select = true },
+            --['<Tab>'] = cmp.mapping.select_next_item(),
+            --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+            -- Manually trigger a completion from nvim-cmp.
+            --  Generally you don't need this, because nvim-cmp will display
+            --  completions whenever it has completion options available.
+            ['<C-Space>'] = cmp.mapping.complete {},
+          },
+          sources = {
+            {
+              name = 'lazydev',
+              -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+              group_index = 0,
+            },
+            { name = 'nvim_lsp' },
+            { name = 'path' },
+          },
+        }
+      end,
+    },
+
     { 'nvim-treesitter', {{
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
@@ -355,6 +418,7 @@ end
 local lspconfig = require('lspconfig');
 local servers = {
   ansiblels = {},
+  eslint = {},
   -- lua_ls = {},
   pyright = {},
   -- rubocop = {},
