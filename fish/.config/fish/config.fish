4c70fd01a65b5bd134317a9c3ecf3580b94fed63
Rohan <code@rohanjain.in>
2025-11-30 09:10:50 +0530

    Debian specific fish configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,12 +5,24 @@ if status is-interactive
   abbr --add j " jrnl"
   abbr --add ts "tmux -u new -As $hostname"
 
+  if type -q batcat
+    # Debian use `batcat` doesn't 
+    set bat_cmd batcat
+  else
+    set bat_cmd bat
+  end
+
+  if type -q fdfind
+    alias fd fdfind
+  end
+
+  alias bat '$bat_cmd --theme ansi'
+  alias batp '$bat_cmd --theme ansi -p'
   alias b biip
   alias re 'exec $SHELL'
   alias rm 'rm -i'
   alias t timew
   alias tw task
-  alias fd fdfind
 
   if type -q starship
     eval (starship init fish)
